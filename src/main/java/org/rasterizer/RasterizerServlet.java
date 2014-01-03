2973afd41ed6f1d00e5ce3f5cdaeaa582b2a339f
Alexander Matyushentsev <matyushentsev@google.com>
2014-01-03 00:38:24 -0800

    Implement base64 encoding, add test page

--- src/main/java/org/rasterizer/RasterizerServlet.java
+++ src/main/java/org/rasterizer/RasterizerServlet.java
@@ -3,10 +3,13 @@ package org.rasterizer;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.PNGTranscoder;
+import org.apache.commons.codec.binary.Base64OutputStream;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,12 +21,20 @@ import javax.servlet.http.HttpServletResponse;
 public class RasterizerServlet extends HttpServlet {
 
   @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    RequestDispatcher view = req.getRequestDispatcher("index.html");
+    view.forward(req, resp);
+  }
+
+  @Override
   protected void doPost(HttpServletRequest req, final HttpServletResponse resp)
       throws ServletException, IOException {
     try {
       String data = req.getParameter("svg");
       String width = req.getParameter("w");
       String height = req.getParameter("h");
+      boolean useBase64 = req.getHeader("HTTP_X_ACCEPT_ENCODING") != null;
       PNGTranscoder transcoder = new PNGTranscoder();
       if (height != null) {
         transcoder.addTranscodingHint(PNGTranscoder.KEY_HEIGHT, new Float(height));
@@ -31,9 +42,17 @@ public class RasterizerServlet extends HttpServlet {
       if (width != null) {
         transcoder.addTranscodingHint(PNGTranscoder.KEY_WIDTH, new Float(width));
       }
+      resp.setContentType("image/png");
+      OutputStream outputStream;
+      if (useBase64) {
+        resp.addHeader("Content-Encoding", "base64");
+        outputStream = new Base64OutputStream(resp.getOutputStream());
+      } else {
+        outputStream = resp.getOutputStream();
+      }
       transcoder.transcode(
           new TranscoderInput(new ByteArrayInputStream(data.getBytes("UTF-8"))),
-          new TranscoderOutput(resp.getOutputStream()));
+          new TranscoderOutput(outputStream));
       resp.setStatus(HttpServletResponse.SC_OK);
     } catch (Exception e) {
       resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
